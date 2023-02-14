936959dc5d28e486b28ff32bfdb7c31b70d642a1
Pablo Palmero <pablorafaelpalmero@gmail.com>
2023-02-14 07:22:22 -0300

    node_modules

---
+++ src/app/content-tab/content-tab.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ContentTabComponent } from './content-tab.component';
+
+describe('ContentTabComponent', () => {
+  let component: ContentTabComponent;
+  let fixture: ComponentFixture<ContentTabComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ContentTabComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ContentTabComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/tab-group-dynamic-example.ts
@@ -0,0 +1,35 @@
+import {Component} from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+/**
+ * @title Tab group with dynamically changing tabs
+ */
+@Component({
+  selector: 'tab-group-dynamic-example',
+  templateUrl: 'tab-group-dynamic-example.html',
+  styleUrls: ['tab-group-dynamic-example.css'],
+})
+export class TabGroupDynamicExample {
+  tabs = ['Ticket N°'];
+  selected = new FormControl(0);
+  nroTicket: number = 0;
+
+  addTab(selectAfterAdding: boolean) { 
+    //consultar número de ticket siguiente
+    this.nroTicket;
+    this.tabs.push('Ticket N°' + this.nroTicket++);
+
+    //if (selectAfterAdding) {
+      this.selected.setValue(this.tabs.length - 1);
+    //}
+  }
+
+  removeTab(index: number) {
+    this.tabs.splice(index, 1);
+  }
+}
+
+
+/**  Copyright 2023 Google LLC. All Rights Reserved.
+    Use of this source code is governed by an MIT-style license that
+    can be found in the LICENSE file at https://angular.io/license */
